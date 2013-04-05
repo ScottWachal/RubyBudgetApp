386b4b26d0631dda1fc236f903e69a0161acebb7
Scott Wachal <scott.wachal@gmail.com>
2013-04-05 15:03:23 -0500

    Account Profile shows index renders now

--- app/controllers/profile_accounts_controller.rb
+++ app/controllers/profile_accounts_controller.rb
@@ -14,6 +14,8 @@ class ProfileAccountsController < ApplicationController
   # GET /profile_accounts/1.json
   def show
     @profile_account  = ProfileAccount.find(params[:id])
+    @envelopes = @profile_account.envelopes
+    @accounts = @profile_account.accounts
     @line_items = @profile_account.line_items
     respond_to do |format|
       format.html # show.html.erb
